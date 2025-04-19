98adf9630050e3947ae4fd4d27cef820477c1487
DESKTOP-NE7TPBC\Amir <kaizamo02@gmail.com>
2025-04-19 13:36:36 +0300

    Добавьте файлы проекта.

---
+++ WpfAppShaitanat/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppShaitanat
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static MainWindow Current;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (Current == null)
+                Current = this;
+        }
+
+        public void Navigate(Page page)
+        {
+            MainFrame.NavigationService.Navigate(page);
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void AppExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Закрыть приложение?", "Подтверждение", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                Application.Current.Shutdown();
+        }
+    }
+}
