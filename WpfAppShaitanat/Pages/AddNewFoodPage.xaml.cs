bc9a59de5a3fa5e7a8fbc506327bbfae3aef64e2
DESKTOP-NE7TPBC\Amir <kaizamo02@gmail.com>
2025-04-19 15:01:46 +0300

    add new pages

---
+++ WpfAppShaitanat/Pages/AddNewFoodPage.xaml.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppShaitanat.DataBase;
+
+namespace WpfAppShaitanat.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddNewFoodPage.xaml
+    /// </summary>
+    public partial class AddNewFoodPage : Page
+    {
+        private byte[] imageBytes;
+
+        public AddNewFoodPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder error = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(FoodPriceTB.Text))
+                error.Append("Цена пустая");
+            if (string.IsNullOrWhiteSpace(FoodNameTB.Text))
+                error.Append("Наименование корма пустое");
+            if (imageBytes == null || imageBytes.Length == 0)
+                error.Append("Фото отсутсвует");
+            if (error.Length > 0)
+            {
+                MessageBox.Show(error.ToString());
+                return;
+            }
+
+            AnimalFood newiten = new AnimalFood()
+            {
+                LastEditDate = DateTime.Now,
+                Price = int.Parse(FoodPriceTB.Text),
+                Name = FoodNameTB.Text,
+                ImageBinary = imageBytes,
+            };
+            App.DB.AnimalFood.Add(newiten);
+
+            App.DB.SaveChanges();
+
+            NavigationService.Navigate(new FoodListPage());
+
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new FoodListPage());
+        }
+
+        private void FoodPriceTb_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!char.IsDigit(e.Text, 0))
+                e.Handled = true;
+        }
+
+        private BitmapImage GetimageSources(byte[] byteImage)
+        {
+            if (byteImage != null)
+            {
+                MemoryStream memoryStream = new MemoryStream(byteImage);
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                image.StreamSource = memoryStream;
+                image.EndInit();
+                return image;
+            }
+            return null;
+        }
+
+        private void EditImaeButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpg)|*.png;*.jpg";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                imageBytes = File.ReadAllBytes(openFileDialog.FileName);
+                tImage.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+            }
+        }
+    }
+}
--- WpfAppShaitanat/Pages/FoodListPage.xaml.cs
+++ WpfAppShaitanat/Pages/FoodListPage.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WpfAppShaitanat.DataBase;
+using WpfAppShaitanat.UserControlls;
 
 namespace WpfAppShaitanat.Pages
 {
@@ -20,9 +13,91 @@ namespace WpfAppShaitanat.Pages
     /// </summary>
     public partial class FoodListPage : Page
     {
+        private List<AnimalFood> allFood;
+        private List<AnimalFood> filteredFood;
+
+        private const int PageSize = 6;
+        private int currentPage = 0;
+
         public FoodListPage()
         {
             InitializeComponent();
+            allFood = App.DB.AnimalFood.ToList();
+            DateFilterCB.SelectedIndex = 0;
+            RefreshList();
+        }
+
+        private void RefreshList()
+        {
+            FoodsWp.Children.Clear();
+            filteredFood = allFood;
+
+            var foodToShow = filteredFood
+                .Skip(currentPage * PageSize)
+                .Take(PageSize)
+                .ToList();
+
+            switch (DateFilterCB.SelectedIndex)
+            {
+                case 1:
+                    foodToShow = foodToShow.OrderBy(x => x.Name).ToList();
+                    break;
+                case 2:
+                    foodToShow = foodToShow.OrderByDescending(x => x.Name).ToList();
+                    break;
+            }
+
+            foreach (var suit in foodToShow)
+            {
+                FoodsWp.Children.Add(new FoodUserControlll(suit));
+            }
+
+            UpdatePageText(foodToShow.Count);
+            UpdateNavigationButtons();
+        }
+
+        private void UpdatePageText(int shownCount)
+        {
+            int startItem = currentPage * PageSize + 1;
+            int endItem = startItem + shownCount - 1;
+            int totalItems = filteredFood.Count;
+
+            PagesTb.Text = $"{startItem}-{endItem} из {totalItems}";
+        }
+
+        private void UpdateNavigationButtons()
+        {
+            ToLeftBtn.IsEnabled = currentPage > 0;
+            ToRightBtn.IsEnabled = (currentPage + 1) * PageSize < allFood.Count();
+        }
+
+        private void PrefPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentPage > 0)
+            {
+                currentPage--;
+                RefreshList();
+            }
+        }
+
+        private void NextPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ((currentPage + 1) * PageSize < allFood.Count())
+            {
+                currentPage++;
+                RefreshList();
+            }
+        }
+
+        private void DateFilterComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            RefreshList();
+        }
+
+        private void AddFoodButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddNewFoodPage());
         }
     }
 }
+
